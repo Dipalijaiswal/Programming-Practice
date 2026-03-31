77e3c1f6d2331215c023ba4517f690ee8238d38d
Dipali Jaiswal <dipalijaiswal098@gmail.com>
2026-03-31 12:59:19 +0530

    Added

---
+++ DSAwithCPP/Tree/HeightofBT.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+using namespace std;
+struct Node{
+    int key;
+    Node*left;
+    Node*right;
+    Node(int k){
+        key=k;
+        left=NULL;
+        right=NULL;
+    }
+};
+int height(Node*root){
+    if(root==NULL){
+        return 0;
+    }
+    else{
+        return (1+max(height(root->left),height(root->right)));
+}
+};
+int main(){
+    Node*root=new Node(1);
+    root->left=new Node(2);
+    root->right=new Node(3);
+    root->right->left=new Node(4);
+    root->right->right=new Node(5);
+    cout<<height(root);
+    return 0;
+}
