86160ff614fa118b10c677a1a6a831d13a5e1df4
Dipali Jaiswal <dipalijaiswal098@gmail.com>
2026-03-31 13:48:45 +0530

    Added

---
+++ DSAwithCPP/Tree/SizeofBT.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+using namespace std;
+struct Node{
+    int key;
+    Node*left;
+    Node*right;
+    Node(int k){
+        key=k;
+        left=right=NULL;
+    }
+};
+int getSize(Node*root){
+    if(root==NULL){
+        return 0;
+    }else{
+        return 1+getSize(root->left)+getSize(root->right);
+    }
+};
+int main(){
+    Node*root=new Node(1);
+    root->left=new Node(2);
+    root->right=new Node(3);
+    root->right->left=new Node(4);
+    root->right->right=new Node(5);
+    cout<<getSize(root);
+    return 0;
+}
