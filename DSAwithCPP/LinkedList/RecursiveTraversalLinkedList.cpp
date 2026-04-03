e60f38e5c2a4c496a132cefac23d92ebe21aefb6
Dipali Jaiswal <dipalijaiswal098@gmail.com>
2026-04-03 20:14:45 +0530

    Added

---
+++ DSAwithCPP/LinkedList/RecursiveTraversalLinkedList.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+struct Node{
+    int data;
+    Node*next;
+};
+void printList(Node*head){
+    if(head==NULL){
+        return;
+    }
+    cout<<head->data<<"";
+    printList(head->next);
+}
+int main(){
+    Node*head=new Node{1,new Node{2,new Node{3,nullptr}}};
+    printList(head);
+    return 0;
+}
