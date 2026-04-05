f561ad3bbbf4f0fcb3251994b4ae97128b7ece0f
Dipali Jaiswal <dipalijaiswal098@gmail.com>
2026-04-05 14:12:38 +0530

    Added

---
+++ DSAwithCPP/Tree/PrintLeftViewBinaryTreeRecrusive.cpp
@@ -0,0 +1,35 @@
+#include<iostream>
+using namespace std;
+struct Node{
+    int key;
+    Node*left;
+    Node*right;
+    Node(int k){
+        key=k;
+        left=NULL;
+        right=NULL;
+    }
+};
+int maxLevel=0;
+void printLeft(Node*root,int level){
+    if(root==NULL)
+    return;
+    if(maxLevel<level){
+        cout<<root->key<<" ";
+        maxLevel=level;
+    }
+    printLeft(root->left,level+1);
+    printLeft(root->right,level+1);
+}
+void printLeftView(Node*root){
+    printLeft(root,1);
+}
+int main(){
+    Node*root=new Node(10);
+    root->left=new Node(20);
+    root->right=new Node(30);
+    root->left->left=new Node(40);
+    root->left->right=new Node(50);
+    printLeftView(root);
+    return 0;
+}
